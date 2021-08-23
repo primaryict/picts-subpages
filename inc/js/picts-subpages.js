8094d3ad9a99acbae64f69b4da6fc48ba18a69e9
primaryict <78017189+primaryict@users.noreply.github.com>
2021-08-23 12:26:50 +0100

    Removed form inc
    Moved all js and css to assets folder.

--- inc/js/picts-subpages.js
+++
@@ -1,7 +0,0 @@
-(function($){
-    $(document).on('click', '.dropDown', function(e){
-	e.preventDefault();
-	$(this).next('.children').slideToggle('fast');
-	$(this).next('.sub-menu').slideToggle('fast');
-    });
-})(jQuery);
